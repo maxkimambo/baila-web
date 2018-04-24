0271038b79501ddaefa81b1c7cb943d75cbb3c03
Max Kimambo <max@kimambo.de>
2018-04-25 00:13:48 +0200

    remove unused vars

--- src/components/App/Root.js
+++ src/components/App/Root.js
@@ -3,7 +3,7 @@ import './App.css';
 import Heading from '../Header/Header';
 import Profile from '../Profile/Profile';
 
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import About from '../About/About'; 
 
 const Root = () => {
