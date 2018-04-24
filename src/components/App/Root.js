8de22924fca6b30095d8238d1fb649806d582187
Max Kimambo <max@kimambo.de>
2018-04-24 23:59:04 +0200

    add deployment configuration

--- src/components/App/Root.js
+++ src/components/App/Root.js
@@ -12,6 +12,28 @@ const Root = () => {
       <Heading />
       <Route exact path='/' component={Profile} />
       <Route path='/about' component={About} /> 
+      {/* Other routes 
+        Viewing a profile
+        /profile/:id 
+        
+           <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+             
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Matches</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Messages</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Profile</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Settings</a>
+                </li> 
+                
+      */}
   </div>
 )
 }
