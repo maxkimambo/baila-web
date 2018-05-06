37c22f760e5cc36afcee0bab8be7d892e2ee1961
Max Kimambo <max@kimambo.de>
2018-05-06 16:51:12 +0200

    range sanity check created

--- src/Reducers/settingsReducer.js
+++ src/Reducers/settingsReducer.js
@@ -15,23 +15,48 @@ const  genderValues = [
     {name: "Female", value:"f"}
 ]
 
-
+/**
+ * Gets a valid max number between two parameters
+ * Used to make sure that lower range is never greater
+ * than upper range
+ * @param {string} val1 
+ * @param {string} val2 
+ */
+const getMax= (val1, val2) => {
+    
+    const first =  parseInt(val1, 10); 
+    const second = parseInt(val2, 10);
+  
+    if(!(isNaN(first) || isNaN(second))){
+        const result = Math.max(first, second);   
+        return result.toString(); 
+    } else {
+       
+        return first.toString(); 
+    }
+   
+}
 const settingsReducer = (state ={}, action) => {
    
     switch(action.type){
         case actionType.LOAD_SETTINGS: {
             const heightValues = generateValues(130,240,5); 
             const ageValues = generateValues(18,99); 
-            return {...state, heightValues, ageValues, genderValues}
+            return Object.assign({}, state.settings, {heightValues}, {ageValues}, {genderValues})
         }
         case actionType.UPDATE_SETTINGS: {
+            let settings = action.settings; 
             
-            return {...state, ...action.settings}
+            settings.alert = 'info'; 
+            settings.alertMessage = 'Preferences updated'; 
+            settings.ageEnd = getMax(settings.ageStart, settings.ageEnd)
+            settings.heightEnd = getMax(settings.heightStart, settings.heightEnd); 
+          
+            return Object.assign({}, state, settings);
         }
         default: 
-            const heightValues = generateValues(130,240,5); 
-            const ageValues = generateValues(18,99); 
-            return {...state, heightValues, ageValues, genderValues}
+           
+            return state; 
     }
  }
 
